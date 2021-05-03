481ce936f71890ce44aba7b91efe41badd70d205
ACinnamonBeard <acinnamonbeard@gmail.com>
2021-05-03 14:41:33 +1000

    fixed modal popup for each button

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,29 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn");
+// Get the buttons that open the modal
+var buttons = document.getElementsByClassName("button");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// Check each button
+for (var i=0; i<buttons.length; i++) {
+	var btn = buttons[i]; 
+	// When the user clicks on the button, open the modal
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
 }
